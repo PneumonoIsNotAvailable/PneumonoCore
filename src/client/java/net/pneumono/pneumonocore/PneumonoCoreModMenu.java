e83494a33c3ead151d5fa2b73496b13754371672
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2024-12-22 13:35:24 +0000

    Update mod menu

--- src/client/java/net/pneumono/pneumonocore/PneumonoCoreModMenu.java
+++ src/client/java/net/pneumono/pneumonocore/PneumonoCoreModMenu.java
@@ -1,20 +1,17 @@
 package net.pneumono.pneumonocore;
 
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.pneumono.pneumonocore.config.ConfigOptionsScreen;
 import net.pneumono.pneumonocore.config.Configs;
 
-public class PneumonoCoreModMenu /*implements ModMenuApi*/ {
-    /*
-    removed until it updates to 1.21.3
-     */
-
-
-    /*@Override
+public class PneumonoCoreModMenu implements ModMenuApi {
+    @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return getModConfigScreenFactory(PneumonoCore.MOD_ID);
     }
 
     public static ConfigScreenFactory<?> getModConfigScreenFactory(String modID) {
         return Configs.hasConfigs(modID) ? parent -> new ConfigOptionsScreen(parent, modID) : null;
-    }*/
+    }
 }
