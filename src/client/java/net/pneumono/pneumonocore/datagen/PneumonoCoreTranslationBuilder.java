33de52af35d2238ad709878da3066af57a2831cd
Pneumono_ <pneumono@zohomail.com>
2025-06-17 15:12:47 +0100

    Add PneumonoCoreTranslationBuilder

---
+++ src/client/java/net/pneumono/pneumonocore/datagen/PneumonoCoreTranslationBuilder.java
@@ -0,0 +1,115 @@
+package net.pneumono.pneumonocore.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.stat.StatType;
+import net.minecraft.util.Identifier;
+import net.pneumono.pneumonocore.config.AbstractConfiguration;
+import net.pneumono.pneumonocore.config.EnumConfiguration;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+@SuppressWarnings("unused")
+public class PneumonoCoreTranslationBuilder {
+    private final FabricLanguageProvider.TranslationBuilder builder;
+
+    public PneumonoCoreTranslationBuilder(FabricLanguageProvider.TranslationBuilder builder) {
+        this.builder = builder;
+    }
+
+    public void add(String key, String value) {
+        this.builder.add(key, value);
+    }
+
+    public void add(Item item, String value) {
+        this.builder.add(item, value);
+    }
+
+    public void add(Block block, String value) {
+        this.builder.add(block, value);
+    }
+
+    public void add(EntityType<?> type, String value) {
+        this.builder.add(type, value);
+    }
+
+    public void add(StatType<?> type, String value) {
+        this.builder.add(type, value);
+    }
+
+    public void add(StatusEffect effect, String value) {
+        this.builder.add(effect, value);
+    }
+
+    public void add(Path existingLangFile) throws IOException {
+        this.builder.add(existingLangFile);
+    }
+
+    public void addItemGroup(RegistryKey<ItemGroup> key, String value) {
+        this.builder.add(key, value);
+    }
+
+    public void addEnchantment(RegistryKey<Enchantment> key, String value) {
+        this.builder.addEnchantment(key, value);
+    }
+
+    public void addEntityAttribute(RegistryEntry<EntityAttribute> entry, String value) {
+        this.builder.add(entry, value);
+    }
+
+    public void add(Identifier key, String value) {
+        this.builder.add(key, value);
+    }
+
+    public void add(Identifier key, String value, String prefix) {
+        this.builder.add(key.toTranslationKey(prefix), value);
+    }
+
+    public void add(Identifier key, String value, String prefix, String suffix) {
+        this.builder.add(key.toTranslationKey(prefix, suffix), value);
+    }
+
+    public void addSubtitle(SoundEvent sound, String subtitle) {
+        Identifier id = sound.id();
+        this.builder.add(id.getNamespace() + ".subtitles." + id.getPath(), subtitle);
+    }
+
+    public void addAdvancement(Identifier id, String name, String description) {
+        this.builder.add(id.toTranslationKey("advancements", "name"), name);
+        this.builder.add(id.toTranslationKey("advancements", "description"), description);
+    }
+
+    public void addConfigScreenTitle(String modId, String title) {
+        this.builder.add("configs." + modId + ".screen_title", title);
+    }
+
+    public void addConfig(AbstractConfiguration<?> config, String name, String tooltip) {
+        this.builder.add(config.getTranslationKey(), name);
+        this.builder.add(config.getTranslationKey() + "tooltip", tooltip);
+    }
+
+    public <T extends Enum<T>> void addEnumConfig(EnumConfiguration<T> config, String name, String tooltip, String... values) {
+        this.addConfig(config, name, tooltip);
+        T[] keys = config.getDefaultValue().getDeclaringClass().getEnumConstants();
+        if (keys.length != values.length) throw new IllegalArgumentException("The number of enum values and translation strings must match!");
+        for (int i = 0; i < keys.length; ++i) {
+            this.builder.add(config.getTranslationKey() + "." + keys[i].toString().toLowerCase(), values[i]);
+        }
+    }
+
+    public <T> BiConsumer<T, String> createBuilder(Function<T, String> keyFunction) {
+        return (object, value) -> this.builder.add(keyFunction.apply(object), value);
+    }
+}
