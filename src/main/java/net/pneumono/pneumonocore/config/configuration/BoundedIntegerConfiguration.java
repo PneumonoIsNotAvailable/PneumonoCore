3343bbc15209010b6c733e5469c133fb92e77e68
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-07-14 22:21:19 +0100

    Create BoundedConfiguration class

--- src/main/java/net/pneumono/pneumonocore/config/configuration/BoundedIntegerConfiguration.java
+++ src/main/java/net/pneumono/pneumonocore/config/configuration/BoundedIntegerConfiguration.java
@@ -2,29 +2,9 @@ package net.pneumono.pneumonocore.config.configuration;
 
 import com.mojang.serialization.Codec;
 
-import java.util.function.Predicate;
-
-public class BoundedIntegerConfiguration extends AbstractConfiguration<Integer> {
-    private final int minValue;
-    private final int maxValue;
-
+public class BoundedIntegerConfiguration extends BoundedConfiguration<Integer> {
     public BoundedIntegerConfiguration(int defaultValue, int minValue, int maxValue, Settings settings) {
-        super(defaultValue, getValidator(minValue, maxValue), settings);
-        this.minValue = minValue;
-        this.maxValue = maxValue;
-    }
-
-    private static Predicate<Integer> getValidator(int minValue, int maxValue) {
-        if (minValue > maxValue) throw new IllegalArgumentException("minValue cannot be larger than maxValue!");
-        return value -> value >= minValue && value <= maxValue;
-    }
-
-    public int getMinValue() {
-        return minValue;
-    }
-
-    public int getMaxValue() {
-        return maxValue;
+        super(defaultValue, minValue, maxValue, settings);
     }
 
     @Override
