5614c1bd3a3b10216ad6e5f51f5a2c5467a88cf8
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-07-18 18:16:25 +0100

    Add CodecUtil (I swear this is relevant)

---
+++ src/main/java/net/pneumono/pneumonocore/util/CodecUtil.java
@@ -0,0 +1,61 @@
+package net.pneumono.pneumonocore.util;
+
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
+import net.minecraft.util.dynamic.Codecs;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CodecUtil {
+    /**
+     * Creates a codec for an enum.
+     *
+     * <p>{@link net.minecraft.util.StringIdentifiable#createCodec} should be used whenever possible,
+     * but if the enum does not implement StringIdentifiable, then this can be used in instead.
+     */
+    public static <T extends Enum<T>> Codec<T> createEnumCodec(T[] enums) {
+        Function<String, T> function;
+        if (enums.length > 16) {
+            Map<String, T> map = Arrays.stream(enums)
+                    .collect(Collectors.toMap(Enum::name, value -> value));
+            function = name -> name == null ? null : map.get(name);
+        } else {
+            function = name -> {
+                for (T constant : enums) {
+                    if (constant.name().equals(name)) {
+                        return constant;
+                    }
+                }
+
+                return null;
+            };
+        }
+
+        return new EnumCodec<>(enums, function);
+    }
+
+    public static class EnumCodec<E extends Enum<E>> implements Codec<E> {
+        private final Codec<E> codec;
+
+        public EnumCodec(E[] values, Function<String, E> idToConstant) {
+            this.codec = Codecs.orCompressed(
+                    Codec.stringResolver(Enum::name, idToConstant),
+                    Codecs.rawIdChecked(Enum::ordinal, ordinal -> ordinal >= 0 && ordinal < values.length ? values[ordinal] : null, -1)
+            );
+        }
+
+        @Override
+        public <T> DataResult<Pair<E, T>> decode(DynamicOps<T> ops, T input) {
+            return this.codec.decode(ops, input);
+        }
+
+        public <T> DataResult<T> encode(E stringIdentifiable, DynamicOps<T> dynamicOps, T object) {
+            return this.codec.encode(stringIdentifiable, dynamicOps, object);
+        }
+    }
+}
