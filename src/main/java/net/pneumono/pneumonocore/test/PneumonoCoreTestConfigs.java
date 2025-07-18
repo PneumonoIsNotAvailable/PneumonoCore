02caae6d613e3d8f7a22b3c9532c6abc4690b16a
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-07-18 21:50:48 +0100

    Add PerPlayerConfiguration
    Missing screen entry because... yeah

--- src/main/java/net/pneumono/pneumonocore/test/PneumonoCoreTestConfigs.java
+++ src/main/java/net/pneumono/pneumonocore/test/PneumonoCoreTestConfigs.java
@@ -2,6 +2,9 @@ package net.pneumono.pneumonocore.test;
 
 import net.pneumono.pneumonocore.PneumonoCore;
 import net.pneumono.pneumonocore.config.*;
+import net.pneumono.pneumonocore.config_api.ConfigApi;
+import net.pneumono.pneumonocore.config_api.configurations.ConfigSettings;
+import net.pneumono.pneumonocore.config_api.configurations.PerPlayerConfiguration;
 
 /**
  * This only exists for testing, don't touch anything here as it won't be registered!
@@ -19,7 +22,13 @@ public class PneumonoCoreTestConfigs {
     public static final TimeConfiguration TIME = new TimeConfiguration(MOD_ID, "test_time", ConfigEnv.SERVER, TimeUnit.MINUTES.getDivision() * 22L);
     public static final BooleanConfiguration CLIENT = new BooleanConfiguration(MOD_ID, "test_client", ConfigEnv.CLIENT, true);
 
+    public static final PerPlayerConfiguration<Boolean> PER_PLAYER = new PerPlayerConfiguration<>(
+            new net.pneumono.pneumonocore.config_api.configurations.BooleanConfiguration(true, new ConfigSettings())
+    );
+
     public static void registerTestConfigs() {
+        ConfigApi.register(PneumonoCore.identifier("per_player"), PER_PLAYER);
+
         Configs.register(MOD_ID,
                 BOOLEAN,
                 ENUM,
