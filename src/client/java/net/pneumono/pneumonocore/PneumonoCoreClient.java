911a9a335d92d81f898acf45681a87f33dad3e4f
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-07-18 19:13:05 +0100

    Reorganise client init

--- src/client/java/net/pneumono/pneumonocore/PneumonoCoreClient.java
+++ src/client/java/net/pneumono/pneumonocore/PneumonoCoreClient.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.pneumono.pneumonocore.config_api.ClientConfigApi;
 import net.pneumono.pneumonocore.config_api.ClientConfigCommandRegistry;
+import net.pneumono.pneumonocore.config_api.ConfigsListWidget;
 import net.pneumono.pneumonocore.config_api.entries.*;
 import net.pneumono.pneumonocore.config_api.ConfigSyncS2CPayload;
 
@@ -11,14 +12,17 @@ public class PneumonoCoreClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// Config
-
 		ClientPlayNetworking.registerGlobalReceiver(ConfigSyncS2CPayload.ID, (payload, context) -> payload.updateConfigs());
 		ClientConfigCommandRegistry.registerClientConfigCommand();
-		ClientConfigApi.registerConfigEntryType(PneumonoCore.identifier("boolean"), BooleanConfigurationEntry::new);
-		ClientConfigApi.registerConfigEntryType(PneumonoCore.identifier("enum"), EnumConfigurationEntry::new);
-		ClientConfigApi.registerConfigEntryType(PneumonoCore.identifier("integer"), IntegerConfigurationEntry::new);
-		ClientConfigApi.registerConfigEntryType(PneumonoCore.identifier("float"), FloatConfigurationEntry::new);
-		ClientConfigApi.registerConfigEntryType(PneumonoCore.identifier("string"), StringConfigurationEntry::new);
-		ClientConfigApi.registerConfigEntryType(PneumonoCore.identifier("time"), TimeConfigurationEntry::new);
+		registerConfigEntryType("boolean", BooleanConfigurationEntry::new);
+		registerConfigEntryType("enum", EnumConfigurationEntry::new);
+		registerConfigEntryType("integer", IntegerConfigurationEntry::new);
+		registerConfigEntryType("float", FloatConfigurationEntry::new);
+		registerConfigEntryType("string", StringConfigurationEntry::new);
+		registerConfigEntryType("time", TimeConfigurationEntry::new);
+	}
+
+	private static void registerConfigEntryType(String name, ConfigsListWidget.EntryBuilder builder) {
+		ClientConfigApi.registerConfigEntryType(PneumonoCore.identifier(name), builder);
 	}
 }
