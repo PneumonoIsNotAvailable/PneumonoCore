a0fa66a53965aa27bfaefeb91f80184895c51898
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-07-30 22:51:53 +0100

    Abstract mixin class

--- src/client/java/net/pneumono/pneumonocore/mixin/client/BipedEntityModelMixin.java
+++ src/client/java/net/pneumono/pneumonocore/mixin/client/BipedEntityModelMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BipedEntityModel.class)
-public class BipedEntityModelMixin {
+public abstract class BipedEntityModelMixin {
     @Inject(
             method = "positionLeftArm",
             at = @At("HEAD"),
