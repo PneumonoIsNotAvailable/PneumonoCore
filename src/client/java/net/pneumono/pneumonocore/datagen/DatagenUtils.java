4dd9749ff952d050a85453c32612d98107527185
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-09-26 04:55:20 +0100

    Re-add enchantmentCriterion to DatagenUtils

--- src/client/java/net/pneumono/pneumonocore/datagen/DatagenUtils.java
+++ src/client/java/net/pneumono/pneumonocore/datagen/DatagenUtils.java
@@ -12,6 +12,13 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
+import java.util.Arrays;
+
+//? if >=1.21.5 {
+import net.minecraft.predicate.component.ComponentPredicateTypes;
+import net.minecraft.predicate.component.ComponentsPredicate;
+//?}
+
 //? if >=1.20.2 {
 import net.minecraft.advancement.AdvancementEntry;
 //?} else {
@@ -76,11 +83,55 @@ public final class DatagenUtils {
     }
     *///?}
 
-    //? if >=1.20.2 {
+    //? if >=1.21.1 {
     @SafeVarargs
-    @Deprecated
     public static AdvancementCriterion<InventoryChangedCriterion.Conditions> enchantmentCriterion(RegistryEntryLookup<Enchantment> lookup, RegistryKey<Enchantment>... enchantments) {
-        return null;
+        return InventoryChangedCriterion.Conditions.items(
+                //? if >=1.21.5 {
+                ItemPredicate.Builder.create().components(
+                        ComponentsPredicate.Builder.create().partial(
+                                ComponentPredicateTypes.ENCHANTMENTS,
+                                EnchantmentsPredicate.enchantments(
+                                        Arrays.stream(enchantments).map(
+                                                enchantment -> new EnchantmentPredicate(lookup.getOrThrow(enchantment), NumberRange.IntRange.atLeast(1))
+                                        ).toList()
+                                )
+                        ).build()
+                )
+                //?} else {
+                /*ItemPredicate.Builder.create().subPredicate(
+                        ItemSubPredicateTypes.ENCHANTMENTS,
+                        EnchantmentsPredicate.enchantments(
+                                Arrays.stream(enchantments).map(
+                                        enchantment -> new EnchantmentPredicate(lookup.getOrThrow(enchantment), NumberRange.IntRange.atLeast(1))
+                                ).toList()
+                        )
+                )
+                *///?}
+        );
     }
-    //?}
+    //?} else if >=1.20.6 {
+    /*public static AdvancementCriterion<InventoryChangedCriterion.Conditions> enchantmentCriterion(Enchantment... enchantments) {
+        return InventoryChangedCriterion.Conditions.items(
+                ItemPredicate.Builder.create().subPredicate(
+                        ItemSubPredicateTypes.ENCHANTMENTS,
+                        EnchantmentsPredicate.enchantments(
+                                Arrays.stream(enchantments).map(
+                                        enchantment -> new EnchantmentPredicate(enchantment, NumberRange.IntRange.atLeast(1))
+                                ).toList()
+                        )
+                )
+        );
+    }
+    *///?} else {
+    /*public static /^? if >=1.20.2 {^/AdvancementCriterion<InventoryChangedCriterion.Conditions>/^?} else {^//^CriterionConditions^//^?}^/ enchantmentCriterion(Enchantment... enchantments) {
+        ItemPredicate.Builder builder = ItemPredicate.Builder.create();
+
+        for (Enchantment enchantment : enchantments) {
+            builder.enchantment(new EnchantmentPredicate(enchantment, NumberRange.IntRange.atLeast(1)));
+        }
+
+        return InventoryChangedCriterion.Conditions.items(builder.build());
+    }
+    *///?}
 }
