4ed06cf8d550cb976648bda8926edc7496afefe4
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-09-26 07:28:49 +0100

    Switch to Kotlin buildscript
    Really need to learn how this works properly but this is fine for now

---
+++ build.gradle.kts
@@ -0,0 +1,96 @@
+plugins {
+	id("fabric-loom") version "1.11-SNAPSHOT"
+	id("maven-publish")
+}
+
+val javaVersion = if (stonecutter.eval(stonecutter.current.version, ">=1.20.5"))
+				JavaVersion.VERSION_21 else JavaVersion.VERSION_17
+java.targetCompatibility = javaVersion
+java.sourceCompatibility = javaVersion
+
+base.archivesName.set(project.property("archives_base_name") as String)
+version = project.property("mod_version") as String
+group = project.property("maven_group") as String
+
+repositories {
+	// Add repositories to retrieve artifacts from in here.
+	// You should only use this when depending on other mods because
+	// Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
+	// See https://docs.gradle.org/current/userguide/declaring_repositories.html
+	// for more information about repositories.
+	maven("https://maven.terraformersmc.com/")
+
+	maven("https://maven.nucleoid.xyz/")
+}
+
+loom {
+	splitEnvironmentSourceSets()
+
+	mods {
+		create("pneumonocore") {
+			sourceSet(sourceSets["main"])
+			sourceSet(sourceSets["client"])
+		}
+	}
+
+	runConfigs.all {
+		ideConfigGenerated(true)
+	}
+}
+
+dependencies {
+	// To change the versions see the gradle.properties file
+	minecraft("com.mojang:minecraft:${property("minecraft_version")}")
+	mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
+	modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
+
+	// Fabric API
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
+
+	// ModMenu
+	modImplementation("com.terraformersmc:modmenu:${property("modmenu_version")}")
+}
+
+tasks {
+	processResources {
+		inputs.property("version", stonecutter.current.version)
+		inputs.property("supported", project.property("supported_versions"))
+
+		filesMatching("fabric.mod.json") {
+			expand("version" to stonecutter.current.version)
+			expand("supported" to project.property("supported_versions"))
+		}
+	}
+
+	withType<JavaCompile> {
+		val java = if (stonecutter.eval(stonecutter.current.version, ">=1.20.5")) 21 else 17
+		options.release.set(java)
+	}
+
+	java {
+		withSourcesJar()
+	}
+
+	jar {
+		from("LICENSE") {
+			rename { "${it}_${base.archivesName.get()}"}
+		}
+	}
+}
+
+// configure the maven publication
+publishing {
+	publications {
+		create<MavenPublication>("mavenJava") {
+			from(components["java"])
+		}
+	}
+
+	// See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
+	repositories {
+		// Add repositories to publish to here.
+		// Notice: This block does NOT have the same function as the block in the top level.
+		// The repositories here will be used for publishing your artifact, not for
+		// retrieving dependencies.
+	}
+}
