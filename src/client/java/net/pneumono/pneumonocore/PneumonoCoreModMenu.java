dbc5ac2e5c8fa14b08f0f152b85b02509dbc2b15
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2026-02-06 07:42:59 +0000

    Provide config screens for dependencies automatically

--- src/client/java/net/pneumono/pneumonocore/PneumonoCoreModMenu.java
+++ src/client/java/net/pneumono/pneumonocore/PneumonoCoreModMenu.java
@@ -2,12 +2,26 @@ package net.pneumono.pneumonocore;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.pneumono.pneumonocore.config_api.ConfigApi;
+import net.pneumono.pneumonocore.config_api.ConfigFile;
 import net.pneumono.pneumonocore.config_api.screen.ClientConfigOptionsScreen;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public final class PneumonoCoreModMenu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return getModConfigScreenFactory(PneumonoCore.MOD_ID);
+    }
+
+    @Override
+    public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
+        Map<String, ConfigScreenFactory<?>> factoryMap = new HashMap<>();
+        for (String namespace : ConfigApi.getConfigFiles().stream().map(ConfigFile::getModId).toList()) {
+            factoryMap.put(namespace, getModConfigScreenFactory(namespace));
+        }
+        return factoryMap;
     }
 
     /**
