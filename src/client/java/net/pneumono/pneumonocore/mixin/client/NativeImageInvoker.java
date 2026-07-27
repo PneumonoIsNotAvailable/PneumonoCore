71de95d55c5b9353002edd5a3d22ffdef3bb82ea
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2026-07-27 20:21:26 +0100

    Add Texture Provider (from Umbrellas)

---
+++ src/client/java/net/pneumono/pneumonocore/mixin/client/NativeImageInvoker.java
@@ -0,0 +1,15 @@
+package net.pneumono.pneumonocore.mixin.client;
+
+import com.mojang.blaze3d.platform.NativeImage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.nio.channels.WritableByteChannel;
+
+@Mixin(NativeImage.class)
+public interface NativeImageInvoker {
+    @Unique
+    @Invoker("writeToChannel")
+    boolean writeToChannel(WritableByteChannel output);
+}
